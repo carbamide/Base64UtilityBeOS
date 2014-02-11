0a691f60c96779d27ce9ccbf6766674a907b8b3e
Josh Barrow <josh@jukaela.com>
2014-02-10 23:18:24 -0600

    Add comments to Base64Window\'s constructor. Further reorganization

--- Base64Window.cpp
+++ Base64Window.cpp
@@ -34,77 +34,99 @@ BMessage Base64Arch;
 
 Base64Window::Base64Window(BRect frame) : BWindow (frame, "Base64 Utility", B_TITLED_WINDOW, B_NOT_RESIZABLE | B_NOT_ZOOMABLE)
 {
+    // Instantiate the main BMenu
 	BMenu *menu;
     
 	menubar = new BMenuBar(Bounds(), "menubar");
 	
+    // Create the File menu
 	menu = new BMenu("File");
 	menu->AddItem(new BMenuItem("Reset", new BMessage(RESET_MESSAGE), 'R'));
 	menu->AddSeparatorItem();
 	menu->AddItem(new BMenuItem("Quit", new BMessage(QUIT_MESSAGE), 'Q'));
 	
+    // Add the File menu to the menu bar
 	menubar->AddItem(menu);
 	
+    // Create the help menu
 	menu = new BMenu("Help");
 	menu->AddItem(new BMenuItem("About...", new BMessage(ABOUT_MESSAGE)));
 	
+    // Add the help menu to the menu bar
 	menubar->AddItem(menu);
 	
+    // Add the menubar to the main BWindow
 	AddChild(menubar);
     
+    // Create the main rect
 	BRect mainRect(frame);
 	mainRect.OffsetTo(0, 0);
     
+    // Create the backgroudn view.  Simply a grey background that things can be added to
 	BView *background = new BView(mainRect, NULL, B_FOLLOW_ALL_SIDES, 0);
     
 	rgb_color color = ui_color(B_PANEL_BACKGROUND_COLOR);
     
+    // No gradients!
 	background->SetViewColor(color);
 	background->SetLowColor(color);
     
 	mainRect.InsetBy(5, 5);
     
+    // Create the inner rects
 	BRect innerRect(mainRect);
 	innerRect.top += 15;
 	innerRect.bottom -= 5;
 	innerRect.right = innerRect.left + 185;
     
+    // Create the BBox that will hold the encoding stuff
 	BBox *encodeBox = new BBox(innerRect, NULL, B_FOLLOW_LEFT | B_FOLLOW_TOP_BOTTOM);
 	encodeBox->SetLabel("Encode");
     
+    // Add it to the background view
 	background->AddChild(encodeBox);
     
+    // Change the inner rect
 	innerRect.left = innerRect.right + 5;
 	innerRect.right = mainRect.right - 5;
 	innerRect.bottom = mainRect.bottom - 5;
     
+    // Create the Bbox that will hold the decoding stuff
 	BBox *decodeBox = new BBox(innerRect, NULL, B_FOLLOW_LEFT | B_FOLLOW_TOP_BOTTOM);
 	decodeBox->SetLabel("Decode");
     
+    // Add it to the background view
 	background->AddChild(decodeBox);
     
+    // Add the background to the main BWindow
 	AddChild(background);
     
+    // Create the rects for the onscreen control.s
 	BRect encodeButtonRect = BRect(10, 15, 175, 30);
 	BRect encodeTextViewRect = BRect(10, 45, 175, 175);
 	BRect decodeButtonRect = BRect(10, 150, 175, 30);
 	BRect decodeTextViewRect = BRect(10, 15, 175, 145);
     
+    // Instantiate the encode button and the decode button
 	encodeButton = new BButton (encodeButtonRect, NULL, "Encode and Display...", new BMessage(ENCODE_BUTTON_MESSAGE));
 	decodeButton = new BButton (decodeButtonRect, NULL, "Decode and Save...", new BMessage(DECODE_BUTTON_MESSAGE));
     
+    // Instantiate the encode text view and decode text view
 	encodeTextView = new BTextView(encodeTextViewRect, "encode_text_view", BRect(0, 0, 175, 175), B_FOLLOW_ALL_SIDES, B_WILL_DRAW | B_PULSE_NEEDED);
 	decodeTextView = new BTextView(decodeTextViewRect, "decode_text_view", BRect(0, 0, 175, 175), B_FOLLOW_ALL_SIDES, B_WILL_DRAW | B_PULSE_NEEDED);
     
+    // Add all the controls to the appropriate boxes
 	encodeBox->AddChild(encodeButton);
 	encodeBox->AddChild(encodeTextView);
 	decodeBox->AddChild(decodeButton);
 	decodeBox->AddChild(decodeTextView);
     
-	BMessenger self(this);
-    
+    // Instantiate the open and save panels
 	openPanel = new BFilePanel(B_OPEN_PANEL, &self, NULL, B_FILE_NODE, false, new BMessage(OPEN_FILE_MESSAGE), NULL);
 	savePanel = new BFilePanel(B_SAVE_PANEL, &self, NULL, B_FILE_NODE, false, new BMessage(SAVE_FILE_MESSAGE), NULL);
+    
+    // Let BMessenger know that we're listening for messages
+	BMessenger self(this);
 };
 
 
